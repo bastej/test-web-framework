d7ae07cf695ede40ae47ebec9eb0f112925e561b
bastej <siatkasebastian@gmail.com>
2020-09-12 16:42:45 +0200

    Implementation of User set method

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { User } from "./User";
 
 const testUser = new User({ name: "Alex", age: 12 });
 
+testUser.set({ age: 23 });
+
 console.log(testUser.get("name"));
 console.log(testUser.get("age"));
