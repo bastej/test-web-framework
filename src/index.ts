16117edef9eba68ea75ee5d08007eaff483e21ef
bastej <siatkasebastian@gmail.com>
2020-09-12 19:46:43 +0200

    Events triggering implementation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import { User } from "./User";
 
 const testUser = new User({ name: "Alex", age: 12 });
 
-testUser.on("change", () => {});
+testUser.on("change", () => {
+  console.log("1");
+});
 
-console.log(testUser);
+testUser.on("change", () => {
+  console.log("2");
+});
+
+testUser.trigger("change");
