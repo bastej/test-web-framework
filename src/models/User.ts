d50583664ba01c0d9c1142a0afc45a9ad5ec6bc8
bastej <siatkasebastian@gmail.com>
2020-09-13 14:33:01 +0200

    Adding User class own fetch method implementation

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from "axios";
 import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
@@ -35,5 +36,17 @@ export class User {
   set(newData: UserProps): void {
     this.attributes.set(newData);
     this.events.trigger("change");
+  }
+
+  fetch(): void {
+    const id = this.get("id");
+
+    if (typeof id !== "number") {
+      throw new Error("Cannot fetch without an id");
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
   }
 }
